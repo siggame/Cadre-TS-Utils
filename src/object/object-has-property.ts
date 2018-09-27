693f8e614111a1aa8eacdfd75f551828ed0fdad5
Jacob Fischer <jacob.t.fischer@gmail.com>
2018-09-26 22:20:51 -0500

    Better type for objectHasProperty

--- src/object/object-has-property.ts
+++ src/object/object-has-property.ts
@@ -1,38 +1,14 @@
 /**
- * Strictly checks for a given key type of a known shape in an object.
- *
- * @param obj - The object to check in.
- * @param property - The name of the property (key) to check for.
- * @returns True if the property is present in the object, false otherwise.
- */
-export function objectHasProperty<T extends object, K extends keyof T>(
-    obj: T,
-    property: K,
-): obj is (T & Required<Pick<T, K>>);
-
-/**
- * Less type safe version for checking if a given object has ANY key.
- *
- * @param obj - The object to check in.
- * @param property - The name of the property (key) to check for.
- * @returns True if the property is present in the object, false otherwise.
- */
-export function objectHasProperty<T extends object>(
-    obj: T,
-    property: string,
-): T;
-
-/**
  * Checks if an object has a given property.
  *
  * @param obj - The object to check in.
  * @param property - The name of the property (key) to check for.
  * @returns True if the property is present in the object, false otherwise.
  */
-export function objectHasProperty<T extends object, K extends keyof T>(
+export function objectHasProperty<T extends object, K extends string>(
     obj: T,
     property: K,
-): obj is (T & Required<Pick<T, K>>) {
+): obj is T & { [P in K]: unknown } {
     return Boolean(obj)
         // tslint:disable-next-line:no-unsafe-any
         && Object.prototype.hasOwnProperty.call(obj, property);
