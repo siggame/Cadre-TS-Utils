4906fadc43ec62ef85f721e58827675ff023d416
Jacob Fischer <jacob.t.fischer@gmail.com>
2018-09-29 22:07:43 -0500

    Remove readonly from base player

--- src/cadre/base-player.ts
+++ src/cadre/base-player.ts
@@ -3,7 +3,7 @@ import { IBaseGameObject } from "./base-game-object";
 /** The base interface that a Player impliments in a game. */
 export interface IBasePlayer extends IBaseGameObject {
     /** The name of the player. */
-    readonly name: string;
+    name: string;
 
     /**
      * What type of client this is,
@@ -11,20 +11,20 @@ export interface IBasePlayer extends IBaseGameObject {
      *
      * @example "Python", "JavaScript", or some other language.
      */
-    readonly clientType: string;
+    clientType: string;
 
     /** The amount of time (in ns) remaining for this AI to send commands. */
-    readonly timeRemaining: number;
+    timeRemaining: number;
 
     /** If the player won the game or not. */
-    readonly won: boolean;
+    won: boolean;
 
     /** If the player lost the game or not. */
-    readonly lost: boolean;
+    lost: boolean;
 
     /** The reason why the player won the game. */
-    readonly reasonWon: string;
+    reasonWon: string;
 
     /** The reason why the player lost the game. */
-    readonly reasonLost: string;
+    reasonLost: string;
 }
