45edb19bf3d3528c1ca0c936556d854491e0cce3
Jacob Fischer <jacob.t.fischer@gmail.com>
2018-10-27 19:58:00 -0500

    Update for npm deployment

--- src/types/shallow-immutable.ts
+++ src/types/shallow-immutable.ts
@@ -3,5 +3,5 @@ import { Primitive } from "./primitive";
 /** Marks everything in the type as readonly. */
 export type ShallowImmutable<T> =
     T extends Primitive ? T :
-        T extends Array<infer U> ? ReadonlyArray<U> :
-            T extends Map<infer K, infer V> ? ReadonlyMap<K, V> : Readonly<T>;
+    T extends Array<infer U> ? ReadonlyArray<U> :
+    T extends Map<infer K, infer V> ? ReadonlyMap<K, V> : Readonly<T>;
