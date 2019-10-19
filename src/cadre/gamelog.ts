2953f7257427c6dcbd502ecdb3ac6d0843ab92f8
Jacob Fischer <jacob.t.fischer@gmail.com>
2019-10-18 20:48:47 -0500

    Update gamelog interfaces to new gamelogVersion field on new gamelogs

--- src/cadre/gamelog.ts
+++ src/cadre/gamelog.ts
@@ -3,7 +3,7 @@ import { IDeltaMergeConstants } from "./delta-merge";
 import { Delta } from "./deltas";
 
 /** The shape of a gamelog, both being built and if read from memory. */
-export interface IGamelog {
+export interface IBaseGamelog {
     /**
      * The name of the game
      *
@@ -18,14 +18,6 @@ export interface IGamelog {
 
     /** The Unix epoch for the time when this gamelog was generated */
     epoch: number;
-
-    /**
-     * Key/Value pairs that describe the settings used to initialize the game.
-     */
-    settings: UnknownObject & {
-        /** The random seed used for server side random logic. */
-        randomSeed: string;
-    };
 
     /** The list of all players that won this game (normally just one). */
     winners: IGamelogWinnerLoser[];
@@ -42,6 +34,44 @@ export interface IGamelog {
      */
     deltas: Delta[];
 }
+
+/** The initial shape for gamelogs in v1 of the game server. */
+export interface IGamelogV0 extends IBaseGamelog {
+    /** The random seed used for server side random logic. */
+    randomSeed: string;
+}
+
+/** The shape of gamelogs before version numbers were recorded as well. */
+export interface IGamelogV1 extends IBaseGamelog {
+    /**
+     * The version of this gamelog, recorded in SemVer.
+     *
+     * Was not recorded initially. Starting version is 2.1.0
+     */
+    gamelogVersion?: string;
+
+    /**
+     * Key/Value pairs that describe the settings used to initialize the game.
+     */
+    settings: UnknownObject & {
+        /** The random seed used for server side random logic. */
+        randomSeed: string;
+    };
+}
+
+/** The shape of gamelogs once version numbers were always recorded. */
+export interface IGamelogV2 extends IGamelogV1 {
+    /**
+     * The version of this gamelog, recorded in SemVer.
+     */
+    gamelogVersion: string;
+}
+
+/** The shape of a gamelog, both being built and if read from memory. */
+export interface IGamelog extends IGamelogV2 {}
+
+/** The shape of a gamelog if you don't know the version, all possible versions */
+export type GamelogUnknownVersion = IGamelogV0 | IGamelogV1 | IGamelogV2;
 
 /** A shorthand object representing a player that won or lost in the game. */
 export interface IGamelogWinnerLoser {
